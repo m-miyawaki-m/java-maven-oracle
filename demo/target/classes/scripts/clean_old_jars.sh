18a553de6564ba3d804a7863248216d9524c6367
miyawaki <121045410+m-miyawaki-m@users.noreply.github.com>
2023-11-19 23:32:58 +0900

    Delete demo/target directory

--- demo/target/classes/scripts/clean_old_jars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-JAR_DIR=/workspaces/java-mysql/demo/work
-JAR_COUNT=$(ls -t $JAR_DIR/*.jar | wc -l)
-if [ $JAR_COUNT -gt 5 ]; then
-  # 古い順に削除
-  ls -tr $JAR_DIR/*.jar | head -n $(($JAR_COUNT-5)) | xargs rm
-fi
